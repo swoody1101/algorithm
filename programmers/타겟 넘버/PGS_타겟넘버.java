e2dd271ccca22a7d99c3b56d9c35a5537a924848
swoody1101@gmail.com <swoody1101@gmail.com>
2024-01-16 10:03:52 +0900

    [PGS] 타겟 넘버 / Lv.2
    https://school.programmers.co.kr/learn/courses/30/lessons/43165

---
+++ programmers/타겟 넘버/PGS_타겟넘버.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class PGS_타겟넘버 {
+    
+    int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, 0, target, 0);
+        return answer;
+    }
+    
+    public void dfs(int[] numbers, int depth, int target, int sum) {
+        if (depth == numbers.length) {
+            if (target == sum) answer++;
+        } else {
+            dfs(numbers, depth + 1, target, sum + numbers[depth]);
+            dfs(numbers, depth + 1, target, sum - numbers[depth]);
+        }
+    }
+}

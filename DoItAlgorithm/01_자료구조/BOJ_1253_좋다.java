ba9def6b012c0ee138f74cd7e672442da93c54b6
이상우 <swoody1101@gmail.com>
2024-09-02 15:32:55 +0900

    [BOJ] 1253번 / 골드4

---
+++ DoItAlgorithm/01_자료구조/BOJ_1253_좋다.java
@@ -0,0 +1,45 @@
+package DataStructure;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1253_좋다 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(nums);
+
+		int answer = 0;
+		for (int i = 0; i < N; i++) {
+			int start = 0;
+			int end = N - 1;
+			while (start < end) {
+				int sum = nums[start] + nums[end];
+				if (sum == nums[i]) {
+					if (i != start && i != end) {
+						answer++;
+						break;
+					} else if (i == start) {
+						start++;
+					} else if (i == end) {
+						end--;
+					}
+				} else if (sum < nums[i]) {
+					start++;
+				} else {
+					end--;
+				}
+			}
+		}
+
+		System.out.println(answer);
+	}
+}

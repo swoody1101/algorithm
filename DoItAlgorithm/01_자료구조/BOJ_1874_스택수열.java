f6414ee4578a5adb2ea4f862639ecc78066f4490
이상우 <swoody1101@gmail.com>
2024-09-03 19:38:32 +0900

    [BOJ] 1874번 스택 수열 / 실버2

--- DoItAlgorithm/01_자료구조/BOJ_1874_스택수열.java
+++ DoItAlgorithm/01_자료구조/BOJ_1874_스택수열.java
@@ -1,7 +1,36 @@
 package DataStructure;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class BOJ_1874_스택수열 {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		StringBuilder sb = new StringBuilder();
+		Stack<Integer> stack = new Stack<Integer>();
+
+		int num = 1;
+		for (int i = 0; i < n; i++) {
+			int target = sc.nextInt();
+
+			while (num <= target && num <= n) {
+				stack.push(num++);
+				sb.append("+\n");
+			}
+
+			if (!stack.isEmpty()) {
+				if (target == stack.peek()) {
+					stack.pop();
+					sb.append("-\n");
+				} else if (target < stack.peek()) {
+					System.out.println("NO");
+					return;
+				}
+			}
+		}
+
+		System.out.println(sb.toString());
 	}
 }
